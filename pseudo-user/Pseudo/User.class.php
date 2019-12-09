21a4dd2baee2f9bfe0e9afb5657ba9c60cdfaf7f
Mark Page <mark.page@burdenandburden.co.uk>
2019-12-09 23:15:33 +0000

    Creates a skeletal starting point

--- pseudo-user/Pseudo/User.class.php
+++ pseudo-user/Pseudo/User.class.php
@@ -15,5 +15,36 @@ class User {
     public function __destruct ( ) {
     }
 
+    public function passwordReset ($answer,$code,$newPassword) {
+        // Reset password after verifying secret answer to question
+        // See \Bab\Agent::passwordReset()
+        return false;
+    }
+
+    public function secretQuestion ($phoneEnd) {
+        // Get a question to provde the secret answer
+        // Constrained by knowing something about user phone number
+        // See \Bab\Agent::secretQuestion()
+        return false;
+    }
+
+    public function token ( ) {
+        // Set and return a token for this user
+        // This is not the hpapi authentication token - this one will be handed to the data provider for use
+        // This function also acts as the hpapi authentication function
+/*
+    Find $this->userId in `pseudo_user`
+    Set a token with an expiry based on a configured token duration (short)
+    Return the token
+*/
+        return false;
+    }
+
+    public function verify ( ) {
+        // Send a verification code to $this->userId
+        // See \Bab\Agent::verify()
+        return false;
+    }
+
 }
 
