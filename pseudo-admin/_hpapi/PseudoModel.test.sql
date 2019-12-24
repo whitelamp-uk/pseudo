c890c9a61a32a1af2a1cc19217e1f2bcddb907c1
Mark Page <mark.page@burdenandburden.co.uk>
2019-12-24 19:08:14 +0000

    Stores more test data

--- pseudo-admin/_hpapi/PseudoModel.test.sql
+++ pseudo-admin/_hpapi/PseudoModel.test.sql
@@ -13,28 +13,33 @@ IF ROW_COUNT() = 1 THEN
 
   SET @userId = LAST_INSERT_ID();
 
-  INSERT INTO `pseudo_column` (`id`, `provider_uuid`, `db_name`, `table_name`, `column_name`) VALUES
-  (1,	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'title'),
-  (2,	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'yob');
+  INSERT INTO `pseudo_column` (`provider_uuid`, `db_name`, `table_name`, `column_name`) VALUES
+  (UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'title'),
+  (UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'yob');
 
-  INSERT INTO `pseudo_provider` (`id`, `uuid`, `name`) VALUES
-  (1,	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'My Organisation');
+  INSERT INTO `pseudo_provider` (`uuid`, `name`) VALUES
+  (UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'My Organisation');
 
-  INSERT INTO `pseudo_read` (`id`, `user_uuid`, `provider_uuid`, `db_name`, `table_name`, `column_name`) VALUES
-  (1,	UNHEX('A0096E271A2211EA94D800165E0004E8'),	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'title'),
-  (2,	UNHEX('A0096E271A2211EA94D800165E0004E8'),	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'yob');
+  INSERT INTO `pseudo_read` (`user_uuid`, `provider_uuid`, `db_name`, `table_name`, `column_name`) VALUES
+  (UNHEX('A0096E271A2211EA94D800165E0004E8'),	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'title'),
+  (UNHEX('A0096E271A2211EA94D800165E0004E8'),	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'yob');
 
   INSERT INTO `pseudo_user` (`user_id`, `uuid`, `remote_addr_pattern`, `token`, `token_expires`) VALUES
   (@userId,	UNHEX('A0096E271A2211EA94D800165E0004E8'),	'.+',	NULL,	NULL);
 
-  INSERT INTO `pseudo_value` (`id`, `uuid`, `provider_uuid`, `db_name`, `table_name`, `column_name`, `decoded_value`) VALUES
-  (1,	UNHEX('7815B5AE267111EA94D800165E0004E8'),	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'title',	'Mr'),
-  (2,	UNHEX('8FE4C38A267111EA94D800165E0004E8'),	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'title',	'Mr'),
-  (3,	UNHEX('A294F51B267111EA94D800165E0004E8'),	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'title',	'Mr'),
-  (4,	UNHEX('CE547C56267111EA94D800165E0004E8'),	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'title',	'Ms'),
-  (5,	UNHEX('E16D2B1D267111EA94D800165E0004E8'),	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'title',	'Mrs'),
-  (6,	UNHEX('197C72B8267211EA94D800165E0004E8'),	UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),	'pseudo_demo',	'person_pseudoed',	'title',	'Miss');
-
+  INSERT INTO `pseudo_value` (`uuid`, `provider_uuid`, `db_name`, `table_name`, `column_name`, `decoded_value`) VALUES
+  (UNHEX('7815B5AE267111EA94D800165E0004E8'),  UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),  'pseudo_demo',  'person_pseudoed',  'title',  'Mr'),
+  (UNHEX('8FE4C38A267111EA94D800165E0004E8'),  UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),  'pseudo_demo',  'person_pseudoed',  'title',  'Mr'),
+  (UNHEX('A294F51B267111EA94D800165E0004E8'),  UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),  'pseudo_demo',  'person_pseudoed',  'title',  'Mr'),
+  (UNHEX('CE547C56267111EA94D800165E0004E8'),  UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),  'pseudo_demo',  'person_pseudoed',  'title',  'Ms'),
+  (UNHEX('E16D2B1D267111EA94D800165E0004E8'),  UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),  'pseudo_demo',  'person_pseudoed',  'title',  'Mrs'),
+  (UNHEX('197C72B8267211EA94D800165E0004E8'),  UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),  'pseudo_demo',  'person_pseudoed',  'title',  'Miss'),
+  (UNHEX('554136D0267F11EA94D800165E0004E8'),  UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),  'pseudo_demo',  'person_pseudoed',  'yob',  '1959'),
+  (UNHEX('5541ED0D267F11EA94D800165E0004E8'),  UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),  'pseudo_demo',  'person_pseudoed',  'yob',  '1965'),
+  (UNHEX('5542EF7F267F11EA94D800165E0004E8'),  UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),  'pseudo_demo',  'person_pseudoed',  'yob',  '1971'),
+  (UNHEX('55439F4A267F11EA94D800165E0004E8'),  UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),  'pseudo_demo',  'person_pseudoed',  'yob',  '1977'),
+  (UNHEX('55443A20267F11EA94D800165E0004E8'),  UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),  'pseudo_demo',  'person_pseudoed',  'yob',  '1983'),
+  (UNHEX('5544DF0B267F11EA94D800165E0004E8'),  UNHEX('8FE3CB9F266E11EA94D800165E0004E8'),  'pseudo_demo',  'person_pseudoed',  'yob',  '1989');
 
 END IF;
 
